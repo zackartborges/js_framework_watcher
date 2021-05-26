7c37643acf8f832e78ea636ba2ac3bdbef62961f
Zack Borges <zackartborges@gmail.com>
2021-05-26 16:12:46 -0400

    adds all stats needed for each framework

--- index.js
+++ index.js
@@ -1,22 +1,93 @@
 /* global axios */
 // Vue Stats
-var response = axios.get("https://api.github.com/repos/vuejs/vue")
-  .then(function (response) {
-    // handle success
-    console.log(response["data"]);
-    // let vueWatchers = (response["data"]["watchers_count"]);
-    // let vueForks = (response["data"]["forks"]);
-    // let vueStars = (response["data"]["stargazers_count"]);
-    // console.log(vueWatchers);
-    // console.log(vueForks);
-    // console.log(vueStars)
+// axios.get("https://api.github.com/repos/vuejs/vue")
+//   .then(function (response) {
+//     // handle success
+//     console.log(response["data"]);
+//     let vueWatchers = (response["data"]["watchers_count"]);
+//     let vueForks = (response["data"]["forks"]);
+//     let vueStars = (response["data"]["stargazers_count"]);
+//     console.log(vueWatchers);
+//     console.log(vueForks);
+//     console.log(vueStars)
 
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-  })
-  .then(function () {
-    // always executed
-  });
-  // stars, watchers, forks
+//   })
+//   .catch(function (error) {
+//     // handle error
+//     console.log(error);
+//   });
+//   // stars, watchers, forks
+// Angular Stats
+  // axios.get("https://api.github.com/repos/angular/angular.js")
+  // .then(function (response) {
+  //   // handle success
+  //   let angularWatchers = (response["data"]["watchers_count"]);
+  //   let angularForks = (response["data"]["forks"]);
+  //   let angularStars = (response["data"]["stargazers_count"]);
+  //   console.log(angularWatchers);
+  //   console.log(angularForks);
+  //   console.log(angularStars)
+  // })
+  // .catch(function (error) {
+  //   // handle error
+  //   console.log(error);
+  // });
+//  Ember Stats
+  // axios.get("https://api.github.com/repos/emberjs/ember.js")
+  // .then(function (response) {
+  //   // handle success
+  //   // console.log(response["data"]);
+  //   let emberWatchers = (response["data"]["watchers_count"]);
+  //   let emberForks = (response["data"]["forks"]);
+  //   let emberStars = (response["data"]["stargazers_count"]);
+  //   console.log(emberWatchers);
+  //   console.log(emberForks);
+  //   console.log(emberStars)
+  // })
+  // .catch(function (error) {
+  //   // handle error
+  //   console.log(error);
+  // })
+  // .then(function () {
+  //   // always executed
+  // });
+
+//  Svelte Stats
+  // axios.get("https://api.github.com/repos/sveltejs/svelte")
+  // .then(function (response) {
+  //   // handle success
+  //   // console.log(response["data"]);
+  //   let svelteWatchers = (response["data"]["watchers_count"]);
+  //   let svelteForks = (response["data"]["forks"]);
+  //   let svelteStars = (response["data"]["stargazers_count"]);
+  //   console.log(svelteWatchers);
+  //   console.log(svelteForks);
+  //   console.log(svelteStars)
+  // })
+  // .catch(function (error) {
+  //   // handle error
+  //   console.log(error);
+  // })
+  // .then(function () {
+  //   // always executed
+  // });
+
+//  React Stats
+  // axios.get("https://api.github.com/repos/facebook/react")
+  // .then(function (response) {
+  //   // handle success
+  //   // console.log(response["data"]);
+  //   let reactWatchers = (response["data"]["watchers_count"]);
+  //   let reactForks = (response["data"]["forks"]);
+  //   let reactStars = (response["data"]["stargazers_count"]);
+  //   console.log(reactWatchers);
+  //   console.log(reactForks);
+  //   console.log(reactStars)
+  // })
+  // .catch(function (error) {
+  //   // handle error
+  //   console.log(error);
+  // })
+  // .then(function () {
+  //   // always executed
+  // });
