e2dd373aa29c693590ffad787cd707dc3e4099bc
Zack Borges <zackartborges@gmail.com>
2021-05-26 15:49:06 -0400

    adds vue statistics from API

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
-const axios = require("axios").default;
-response = axios.get("")
+/* global axios */
+// Vue Stats
+var response = axios.get("https://api.github.com/repos/vuejs/vue")
+  .then(function (response) {
+    // handle success
+    console.log(response["data"]);
+    // let vueWatchers = (response["data"]["watchers_count"]);
+    // let vueForks = (response["data"]["forks"]);
+    // let vueStars = (response["data"]["stargazers_count"]);
+    // console.log(vueWatchers);
+    // console.log(vueForks);
+    // console.log(vueStars)
+
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .then(function () {
+    // always executed
+  });
+  // stars, watchers, forks
