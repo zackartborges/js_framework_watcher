224a5610e472079dbc7c5f166cb98037c775a96b
Zack Borges <zackartborges@gmail.com>
2021-05-28 08:39:16 -0400

    gets working get request function

--- index.js
+++ index.js
@@ -1,22 +1,51 @@
 /* global axios */
 // Vue Stats
-async function getData (url) {
-  var repoData = await axios.get(url)
-    .then(function (response) {
+// async function getData (url) {
+//   await axios.get(url)
+//     .then((response) {
+//       const repoData = {
+//         watchers: response["data"]["watchers_count"],
+//         forks: response["data"]["forks"],
+//         stars: response["data"]["stargazers_count"]
+//       }
+      
+//       return repoData;
+
+//     })
+//     .catch(err => {
+//       console.log(err);
+//     });
+//     ;
+//     // console.log(repoData);
+// };
+
+const sendGetRequest = async (url) => {
+  try {
+      const resp = await axios.get(url);
       const repoData = {
-        watchers: response["data"]["watchers_count"],
-        forks: response["data"]["forks"],
-        stars: response["data"]["stargazers_count"]
+        name: resp["data"]["full_name"],
+        watchers: resp["data"]["watchers_count"],
+        forks: resp["data"]["forks"],
+        stars: resp["data"]["stargazers_count"]
       }
-      return repoData;
-
-    });
-    // console.log(repoData);
+      // console.log(resp.data);
+    console.log(repoData);
+  } catch (err) {
+      // Handle Error Here
+      console.error(err);
+  }
+  // console.log(repoData);
 };
+sendGetRequest("https://api.github.com/repos/vuejs/vue");
+sendGetRequest("https://api.github.com/repos/angular/angular.js");
+sendGetRequest("https://api.github.com/repos/emberjs/ember.js");
+sendGetRequest("https://api.github.com/repos/sveltejs/svelte");
+sendGetRequest("https://api.github.com/repos/facebook/react");
+// sendGetRequest();
 
-var vueData = getData("https://api.github.com/repos/vuejs/vue");
+// var vueData = getData("https://api.github.com/repos/vuejs/vue");
 
-console.log(vueData);
+// console.log(vueData);
 // getRequest("https://api.github.com/repos/vuejs/vue");
 // axios.get("https://api.github.com/repos/vuejs/vue")
 //   .then(function (response) {
