2a7ff9f65c4acab7117273090e509c1dfcd86c2f
Zack Borges <zackartborges@gmail.com>
2021-05-29 10:11:27 -0400

    changes function to .data instead of brakcets

--- index.js
+++ index.js
@@ -3,21 +3,23 @@ const sendGetRequest = async (url) => {
   try {
       const resp = await axios.get(url);
       const repoData = {
-        name: resp["data"]["full_name"],
-        watchers: resp["data"]["watchers_count"],
-        forks: resp["data"]["forks"],
-        stars: resp["data"]["stargazers_count"]
+        name: resp.data.full_name,
+        watchers: resp.data.watchers_count,
+        forks: resp.data.forks,
+        stars: resp.data.stargazers_count
       }
+      // return repoData;
       // console.log(resp.data);
     console.log(repoData);
   } catch (err) {
       // Handle Error Here
       console.error(err);
   }
-  // console.log(repoData);
 };
+//   console.log(vueData)
+// })();
 sendGetRequest("https://api.github.com/repos/vuejs/vue");
-sendGetRequest("https://api.github.com/repos/angular/angular.js");
-sendGetRequest("https://api.github.com/repos/emberjs/ember.js");
-sendGetRequest("https://api.github.com/repos/sveltejs/svelte");
-sendGetRequest("https://api.github.com/repos/facebook/react");
+// const AngularData = sendGetRequest("https://api.github.com/repos/angular/angular.js");
+// const emberData = sendGetRequest("https://api.github.com/repos/emberjs/ember.js");
+// const svelteData = sendGetRequest("https://api.github.com/repos/sveltejs/svelte");
+// const reactData = sendGetRequest("https://api.github.com/repos/facebook/react");
