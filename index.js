b97fe1c820df87b8bbcbb9f07f9c2a170f428af6
Zack Borges <zackartborges@gmail.com>
2021-06-07 14:30:42 -0400

    in process of getting data to display in chart function

--- index.js
+++ index.js
@@ -1,34 +1,93 @@
-/* global axios */
-const sendGetRequest = async (url) => {
-  let repoData;
-  try {
-      const resp = await axios.get(url);
-      const repoData = {
-        name: resp.data.full_name,
-        watchers: resp.data.watchers_count,
-        forks: resp.data.forks,
-        stars: resp.data.stargazers_count
-      }
-      // return repoData;
-      // console.log(resp.data);
-    // console.log(repoData);
-    return repoData
-  } catch (err) {
-      // Handle Error Here
-      console.error(err);
-  }
-};
+// // /* global axios */
+// // Vue STats
+// axios.get("https://api.github.com/repos/vuejs/vue")
+//   .then(function (response) {
+//     // handle success
+//     // console.log(response["data"]);
+//     let vueWatchers = (response["data"]["watchers_count"]);
+//     let vueForks = (response["data"]["forks"]);
+//     let vueStars = (response["data"]["stargazers_count"]);
+//     console.log(vueWatchers);
+//     console.log(vueForks);
+//     console.log(vueStars)
 
-// const vueData =  await sendGetRequest("https://api.github.com/repos/vuejs/vue");
-// console.log(vueData);
-const vueData = (async () => {
-  const getVueData = await sendGetRequest("https://api.github.com/repos/vuejs/vue")
-  console.log(getVueData)
-})()
-console.log(vueData)
-// const vueData = sendGetRequest("https://api.github.com/repos/vuejs/vue");
-// console.log(vueData);
-// const AngularData = sendGetRequest("https://api.github.com/repos/angular/angular.js");
-// const emberData = sendGetRequest("https://api.github.com/repos/emberjs/ember.js");
-// const svelteData = sendGetRequest("https://api.github.com/repos/sveltejs/svelte");
-// const reactData = sendGetRequest("https://api.github.com/repos/facebook/react");
+//   })
+//   .catch(function (error) {
+//     // handle error
+//     console.log(error);
+//   });
+// // stars, watchers, forks
+// // Angular Stats
+// axios.get("https://api.github.com/repos/angular/angular.js")
+//   .then(function (response) {
+//     // handle success
+//     let angularWatchers = (response["data"]["watchers_count"]);
+//     let angularForks = (response["data"]["forks"]);
+//     let angularStars = (response["data"]["stargazers_count"]);
+//     console.log(angularWatchers);
+//     console.log(angularForks);
+//     console.log(angularStars)
+//   })
+//   .catch(function (error) {
+//     // handle error
+//     console.log(error);
+//   });
+// //  Ember Stats
+// axios.get("https://api.github.com/repos/emberjs/ember.js")
+//   .then(function (response) {
+//     // handle success
+//     // console.log(response["data"]);
+//     let emberWatchers = (response["data"]["watchers_count"]);
+//     let emberForks = (response["data"]["forks"]);
+//     let emberStars = (response["data"]["stargazers_count"]);
+//     console.log(emberWatchers);
+//     console.log(emberForks);
+//     console.log(emberStars)
+//   })
+//   .catch(function (error) {
+//     // handle error
+//     console.log(error);
+//   })
+//   .then(function () {
+//     // always executed
+//   });
+
+// //  Svelte Stats
+// axios.get("https://api.github.com/repos/sveltejs/svelte")
+//   .then(function (response) {
+//     // handle success
+//     // console.log(response["data"]);
+//     let svelteWatchers = (response["data"]["watchers_count"]);
+//     let svelteForks = (response["data"]["forks"]);
+//     let svelteStars = (response["data"]["stargazers_count"]);
+//     console.log(svelteWatchers);
+//     console.log(svelteForks);
+//     console.log(svelteStars)
+//   })
+//   .catch(function (error) {
+//     // handle error
+//     console.log(error);
+//   })
+//   .then(function () {
+//     // always executed
+//   });
+
+// //  React Stats
+// axios.get("https://api.github.com/repos/facebook/react")
+//   .then(function (response) {
+//     // handle success
+//     // console.log(response["data"]);
+//     let reactWatchers = (response["data"]["watchers_count"]);
+//     let reactForks = (response["data"]["forks"]);
+//     let reactStars = (response["data"]["stargazers_count"]);
+//     console.log(reactWatchers);
+//     console.log(reactForks);
+//     console.log(reactStars)
+//   })
+//   .catch(function (error) {
+//     // handle error
+//     console.log(error);
+//   })
+//   .then(function () {
+//     // always executed
+//   }); 
