46e20ca630fc350cc136ba02c52c1153855ac35e
Zack Borges <zackartborges@gmail.com>
2021-06-08 17:02:36 -0400

    commits Promiseall working code. Gets all objects data needed

--- index.js
+++ index.js
@@ -1,93 +1,32 @@
-// // /* global axios */
-// // Vue STats
-// axios.get("https://api.github.com/repos/vuejs/vue")
+/* global axios */
+// var response = axios.get("https://api.github.com/repos/vuejs/vue")
 //   .then(function (response) {
 //     // handle success
-//     // console.log(response["data"]);
-//     let vueWatchers = (response["data"]["watchers_count"]);
-//     let vueForks = (response["data"]["forks"]);
-//     let vueStars = (response["data"]["stargazers_count"]);
-//     console.log(vueWatchers);
-//     console.log(vueForks);
-//     console.log(vueStars)
-
-//   })
-//   .catch(function (error) {
-//     // handle error
-//     console.log(error);
-//   });
-// // stars, watchers, forks
-// // Angular Stats
-// axios.get("https://api.github.com/repos/angular/angular.js")
-//   .then(function (response) {
-//     // handle success
-//     let angularWatchers = (response["data"]["watchers_count"]);
-//     let angularForks = (response["data"]["forks"]);
-//     let angularStars = (response["data"]["stargazers_count"]);
-//     console.log(angularWatchers);
-//     console.log(angularForks);
-//     console.log(angularStars)
-//   })
-//   .catch(function (error) {
-//     // handle error
-//     console.log(error);
-//   });
-// //  Ember Stats
-// axios.get("https://api.github.com/repos/emberjs/ember.js")
-//   .then(function (response) {
-//     // handle success
-//     // console.log(response["data"]);
-//     let emberWatchers = (response["data"]["watchers_count"]);
-//     let emberForks = (response["data"]["forks"]);
-//     let emberStars = (response["data"]["stargazers_count"]);
-//     console.log(emberWatchers);
-//     console.log(emberForks);
-//     console.log(emberStars)
-//   })
-//   .catch(function (error) {
-//     // handle error
-//     console.log(error);
-//   })
-//   .then(function () {
-//     // always executed
+//     console.log(response["data"]);
 //   });
 
-// //  Svelte Stats
-// axios.get("https://api.github.com/repos/sveltejs/svelte")
-//   .then(function (response) {
-//     // handle success
-//     // console.log(response["data"]);
-//     let svelteWatchers = (response["data"]["watchers_count"]);
-//     let svelteForks = (response["data"]["forks"]);
-//     let svelteStars = (response["data"]["stargazers_count"]);
-//     console.log(svelteWatchers);
-//     console.log(svelteForks);
-//     console.log(svelteStars)
-//   })
-//   .catch(function (error) {
-//     // handle error
-//     console.log(error);
-//   })
-//   .then(function () {
-//     // always executed
-//   });
+// var response = axios.get("https://api.github.com/repos/vuejs/vue").then(function (response) {
+//   // handle success
+//   console.log(response["data"]);
+// });
 
-// //  React Stats
-// axios.get("https://api.github.com/repos/facebook/react")
-//   .then(function (response) {
-//     // handle success
-//     // console.log(response["data"]);
-//     let reactWatchers = (response["data"]["watchers_count"]);
-//     let reactForks = (response["data"]["forks"]);
-//     let reactStars = (response["data"]["stargazers_count"]);
-//     console.log(reactWatchers);
-//     console.log(reactForks);
-//     console.log(reactStars)
-//   })
-//   .catch(function (error) {
-//     // handle error
-//     console.log(error);
-//   })
-//   .then(function () {
-//     // always executed
-//   }); 
+function getVueData() {
+  return axios.get("https://api.github.com/repos/vuejs/vue");
+}
+function getAngularData() {
+  return axios.get("https://api.github.com/repos/angular/angular.js");
+}
+function getEmberData() {
+  return axios.get("https://api.github.com/repos/emberjs/ember.js");
+}
+function getSvelteData() {
+  return axios.get("https://api.github.com/repos/sveltejs/svelte");
+}
+function getReactData() {
+  return axios.get("https://api.github.com/repos/facebook/react");
+}
+Promise.all([getVueData(), getAngularData(), getEmberData(), getSvelteData(), getReactData()]).then(function (
+  results
+) {
+  console.log(results);
+});
