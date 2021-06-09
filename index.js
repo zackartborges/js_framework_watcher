9879c7e706f4171d5ac7f34bfe3e65dceeb0a366
Zack Borges <zackartborges@gmail.com>
2021-06-09 09:13:29 -0400

    adds repo data specific in promiseAll

--- index.js
+++ index.js
@@ -9,24 +9,32 @@
 //   // handle success
 //   console.log(response["data"]);
 // });
+function getData() {
+  function getVueData() {
+    return axios.get("https://api.github.com/repos/vuejs/vue");
+  }
+  function getAngularData() {
+    return axios.get("https://api.github.com/repos/angular/angular.js");
+  }
+  function getEmberData() {
+    return axios.get("https://api.github.com/repos/emberjs/ember.js");
+  }
+  function getSvelteData() {
+    return axios.get("https://api.github.com/repos/sveltejs/svelte");
+  }
+  function getReactData() {
+    return axios.get("https://api.github.com/repos/facebook/react");
+  }
+  Promise.all([getVueData(), getAngularData(), getEmberData(), getSvelteData(), getReactData()]).then(function (
+    results
+  ) {
+    let vueData = results[0];
+    let angularData = results[1];
+    let emberData = results[2];
+    let svelteData = results[3];
+    let reactData = results[4];
+    console.log(results);
+  });
+}
 
-function getVueData() {
-  return axios.get("https://api.github.com/repos/vuejs/vue");
-}
-function getAngularData() {
-  return axios.get("https://api.github.com/repos/angular/angular.js");
-}
-function getEmberData() {
-  return axios.get("https://api.github.com/repos/emberjs/ember.js");
-}
-function getSvelteData() {
-  return axios.get("https://api.github.com/repos/sveltejs/svelte");
-}
-function getReactData() {
-  return axios.get("https://api.github.com/repos/facebook/react");
-}
-Promise.all([getVueData(), getAngularData(), getEmberData(), getSvelteData(), getReactData()]).then(function (
-  results
-) {
-  console.log(results);
-});
+getData();
